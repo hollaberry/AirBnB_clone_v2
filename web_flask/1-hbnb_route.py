02d670a4ebf30eba9adee316dcb51a8097116f12
olatunde <lawalolatunde@ymail.com>
2022-04-14 09:06:28 -0700

    task 2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""Script that start a Flask web application """
+"""
+Module 1-hello_route.Script that start a
+Flask web application Test on tab:python3
+-m web_flask.1-hello_route
+"""
 
 from flask import Flask
 app = Flask(__name__)
 app.url_map..strict_slashes = False
 
+
 @app.route("/")
 def hello_hbnb():
     """
-    route function 
+    route function
     that displays string on url page
     """
     return ("Hello HBNB!")
+
 
 @app.route("/hbnb")
 def hbnb():
@@ -20,6 +26,7 @@ def hbnb():
     is used in url
     """
     return ("HBNB")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
