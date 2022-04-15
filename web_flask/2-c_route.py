31a424f50122cd3cfe80672f431340021864ed43
olatunde <lawalolatunde@ymail.com>
2022-04-15 07:24:28 -0700

    news

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-"""Script that start a Flask web application """
+"""
+Module 2-c_route.Script that start a
+Flask web application Test on tab:python3
+-m web_flask.2-c_route
+On another tab:curl 0.0.0.0:5000/hbnb ; echo "" | cat -e
+There must be a value in <text> otherwise return 404
+"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -13,6 +19,7 @@ def hello_hbnb():
     """
     return ("Hello HBNB!")
 
+
 @app.route("/hbnb")
 def hbnb():
     """ 
@@ -20,6 +27,7 @@ def hbnb():
     is used in url
     """
     return ("HBNB")
+
 
 @app.route("/c/<text")
 def c_is_fun(text):
@@ -31,4 +39,4 @@ def c_is_fun(text):
     return ("C {}".format(text.replace('_', ' ')))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
