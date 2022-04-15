fadba41578bf4983e68e10b4c705b485f60c1063
olatunde <lawalolatunde@ymail.com>
2022-04-15 07:47:59 -0700

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,53 +1,61 @@
 #!/usr/bin/python3
-"""Script that start a Flask web application """
+"""
+Module 1-hello_route.Script that start a
+Flask web application Test on tab:python3
+-m web_flask.1-hello_route
+On another tab:curl 0.0.0.0:5000/hbnb ; echo "" | cat -e
+"""
 
 from flask import Flask
 app = Flask(__name__)
-app.url_map..strict_slashes = False
+app.url_map.strict_slashes = False
+
 
 @app.route("/")
 def hello_hbnb():
     """
-    route function 
+    route function
     that displays string on url page
     """
     return ("Hello HBNB!")
 
+
 @app.route("/hbnb")
 def hbnb():
-    """ 
+    """
     displays HBNB when /hbnb
     is used in url
     """
     return ("HBNB")
 
-@app.route("/c/<text")
+
+@app.route("/c/<text>")
 def c_is_fun(text):
     """
-    Displays C followed by value of text
-    replace _ sysmbols with a space
-    without the text it returns 404
+    Display C then text variable
+    if text valueless return 404
     """
     return ("C {}".format(text.replace('_', ' ')))
+
 
 @app.route("/python")
 @app.route("/python/<text>")
 def python_is_cool(text="is cool"):
     """
-    display "Python" followed by text variable
-    text variable. replace _ with space
-    The default value of text is "is cool"
+    display python, the text value
+    return is cool as text default
     """
     return ("Python {}".format(text.replace('_', ' ')))
 
-@app.route("/number/<int:n>")
+
+@app.route("/number/int:<n>")
 def is_a_number(n):
     """
-    display n is a number
-    only if n is an integer
+    display n is a number only
+    if n is an integer
     """
     return ("{} is a number".format(n))
 
-    
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
