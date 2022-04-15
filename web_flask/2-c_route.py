6dd476ca9b4cbba0d5a797fe6d1a8241c75c25a2
olatunde <lawalolatunde@ymail.com>
2022-04-15 07:33:18 -0700

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-Module 2-c_route.Script that start a
+Module 1-hello_route.Script that start a
 Flask web application Test on tab:python3
--m web_flask.2-c_route
+-m web_flask.1-hello_route
 On another tab:curl 0.0.0.0:5000/hbnb ; echo "" | cat -e
-There must be a value in <text> otherwise return 404
 """
 
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
 
 @app.route("/")
 def hello_hbnb():
@@ -22,21 +22,21 @@ def hello_hbnb():
 
 @app.route("/hbnb")
 def hbnb():
-    """ 
+    """
     displays HBNB when /hbnb
     is used in url
     """
     return ("HBNB")
 
 
-@app.route("/c/<text")
+@app.route("/c/<text>")
 def c_is_fun(text):
     """
-    Displays C followed by value of <text>
-    replace _ sysmbols with a space
-    without the text it returns 404
+    Display C then text variable
+    if text valueless return 404
     """
     return ("C {}".format(text.replace('_', ' ')))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
